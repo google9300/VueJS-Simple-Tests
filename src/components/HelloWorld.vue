63f64b283abe5e3d510661228b04f5f5c256ff2a
Ventzeslav Petrov <vpetrotc@training.local>
2019-04-04 19:55:38 +0300

    Uploading Dummy Project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <p :class="{active: isActive}">Name</p>
+    <p>{{isActive ? 'Active' : 'Not Active'}}</p>
+    <button :class="{active: isActive}" @click="isActive=!isActive">CLICK ME</button>
+    <br>
+    <p :class="{active: isActive}" v-if="visible">First Paragraph</p>
+    <p :class="{active: isActive}">Secound Paragraph</p>
+    <button v-on:click="visible=!visible">{{visible?'Hide':'Show'}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: function() {
+    return { isActive: false, visible: false };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  background-color: black;
+  color: white;
+}
+p.active {
+  background-color: yellowgreen;
+  color: red;
+}
+</style>
